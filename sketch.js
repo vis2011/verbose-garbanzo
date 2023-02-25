532091300f9eb03279119f5cbb28dc9dbcfc4bdf
vis2011 <75988290+vis2011@users.noreply.github.com>
2023-02-25 17:56:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const hamburger = document.querySelector('.hamburger');
+  const menu = document.querySelector('.menu');
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+}
+
+
